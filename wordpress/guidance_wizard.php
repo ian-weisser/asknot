41e40c82192dde455d710b1998e39a687fe50efb
Ian Weisser <ian-weisser@ubuntu.com>
2014-12-24 10:49:58 -0600

    Convert span and div tags to p and h3 tags. The tags will inherit from parent-theme CSS.

--- wordpress/guidance_wizard.php
+++ wordpress/guidance_wizard.php
@@ -1,181 +1,182 @@
-<div id="volunteer_wizard"> <!-- The entire wizard is inside this div-->
 
-  <div class="group" id="toplevel">
-    <span class="question">How would you like to help?<br/>What scratches your itch?</span>
-    <ul class="choices">
-      <li next-group="advocate">Advocacy</li>
-      <li next-group="communicate">Writing and editing</li>
-      <li next-group="develop">Coding and development</li>
-      <li next-group="quality">Quality and design</li>
-      <li next-group="support">Support</li>
-    </ul>
-  </div>
+  <div id="volunteer_wizard">
 
-  <div class="group" id="advocate">
-    <span class="question">So you want to well the world about Ubuntu?<br/>You could help</span>
-    <ul class="choices">
-      <li target="http://www.spreadubuntu.org/en/spreadubuntu-our-project">Create
-        <div class="extra">Create materials for the Spreadubuntu project</div>
-      </li>
-      <li target="http://www.spreadubuntu.org/">Persuade
-        <div class="extra">Evangelize with the Spreadubuntu project</div>
-      </li>
-      <li target="https://wiki.ubuntu.com/StartUbuntu#HOWTO_Help.3F">Convert WinXP users
-        <div class="extra">Evangelize with the Startubuntu project</div>
-      </li>
-    </ul>
-  </div>
-
-
-
-  <div class="group" id="communicate">
-    <span class="question">So you have a mighty pen?<br/>You could help</span>
-    <ul class="choices">
-      <li target="https://wiki.ubuntu.com/UbuntuWeeklyNewsletter/Join">Interview
-        <div class="extra">Get the message across in the Ubuntu Weekly News</div>
-      </li>
-      <li target="https://wiki.ubuntu.com/Classroom">Teach
-        <div class="extra">Share your knowledge and skills in Ubuntu Classroom</div>
-      </li>
-      <li target="http://community.ubuntu.com/contribute/translations/">Translate
-        <div class="extra">Help make Ubuntu accessible to everyone</div>
-      </li>
-      <li target="https://wiki.ubuntu.com/DocumentationTeam">Edit and update documentation
-        <div class="extra">with the Ubuntu Documentation Team</div>
-      </li>
-      <li target="https://wiki.ubuntu.com/UbuntuWeeklyNewsletter/Join">Write/edit news copy
-        <div class="extra">for the Ubuntu Weekly News</div>
-      </li>
-      <li target="https://wiki.ubuntu.com/UbuntuWeeklyNewsletter/Join">Write/edit news copy
-        <div class="extra">for the Ubuntu Weekly News</div>
-      </li>
-      <li target="http://ubuntuforums.org/forumdisplay.php?f=100">Compile tutorials
-        <div class="extra">for the Ubuntu Forums</div>
-      </li>
-      <li target="http://askubuntu.com/">Edit
-        <div class="extra">questions and answers at AskUbuntu</div>
-      </li>
-    </ul>
-  </div>
-
-
-  <div class="group" id="develop">
-    <span class="question">So you want to change the world with great software?<br/>You could help</span>
-    <ul class="choices">
-      <li target="https://wiki.ubuntu.com/QATeam/Roles/BugTriager">Triage bugs
-        <div class="extra">with the Ubuntu QA Team</div>
-      </li>
-      <li target="https://wiki.ubuntu.com/One%20Hundred%20Papercuts">Patch simple bugs
-        <div class="extra">with the One Hundred Papercuts Project</div>
-      </li>
-      <li target="http://developer.ubuntu.com/">Develop applications
-        <div class="extra">for Cloud, Server, Desktop, Tablet, and/or Phone</div>
-      </li>
-      <li target="https://wiki.ubuntu.com/FoundationsTeam">Improve the core OS
-        <div class="extra">with the Ubuntu Foundations Team</div>
-      </li>
-      <li target="https://wiki.ubuntu.com/QATeam/Roles/Developer">Create infrastructure tools
-        <div class="extra">with the Ubuntu QA Team</div>
-      </li>
-      <li target="http://packaging.ubuntu.com/">Package software
-        <div class="extra">to add to Ubuntu</div>
-      </li>
-      <li target="https://wiki.ubuntu.com/SecurityTeam/GettingInvolved">Protect millions
-        <div class="extra">with the Ubuntu Security Team</div>
-      </li>
-      <li target="https://wiki.ubuntu.com/QATeam/Roles/TestWriter">Write automated tests
-        <div class="extra">with the Ubuntu QA Team</div>
-      </li>
-      <li target="https://wiki.ubuntu.com/QATeam/Roles/Tester">Test the development release
-        <div class="extra">with the Ubuntu QA Team</div>
-      </li>
-      <li target="http://community.ubuntu.com/contribute/design/">Visual experience
-        <div class="extra">Wallpapers, art, and user experience design</div>
-      </li>
-    </ul>
-  </div>
-
-
-  <div class="group" id="quality">
-    <span class="question">So you enjoy finding programmer mistakes?<br/>You could help</span>
-    <ul class="choices">
-      <li target="https://wiki.ubuntu.com/FoundationsTeam">Improve the core OS
-        <div class="extra">with the Ubuntu Foundations Team</div>
-      </li>
-      <li target="http://community.ubuntu.com/contribute/design/">User experience
-        <div class="extra">Designing for the Desktop, Tablet, and Phone</div>
-      </li>
-      <li target="https://wiki.ubuntu.com/QATeam/Roles/Developer">Create infrastructure tools
-        <div class="extra">with the Ubuntu QA Team</div>
-      </li>
-      <li target="https://wiki.ubuntu.com/SecurityTeam/GettingInvolved">Protect millions
-        <div class="extra">with the Ubuntu Security Team</div>
-      </li>
-      <li target="https://wiki.ubuntu.com/QATeam/Roles/TestWriter">Write automated tests
-        <div class="extra">with the Ubuntu QA Team</div>
-      </li>
-      <li target="https://wiki.ubuntu.com/QATeam/Roles/Tester">Test the development release
-        <div class="extra">with the Ubuntu QA Team</div>
-      </li>
-      <li target="https://wiki.ubuntu.com/QATeam/Roles/BugTriager">Triage bugs
-        <div class="extra">with the Ubuntu QA Team</div>
-      </li>
-    </ul>
-  </div>
-
-
-  <div class="group" id="support">
-    <span class="question">So you like helping people?<br/>You could help</span>
-    <ul class="choices">
-      <li target="http://ubuntuforums.org/forumdisplay.php?f=100">Compile tutorials
-        <div class="extra">for the Ubuntu Forums</div>
-      </li>
-      <li target="http://ubuntuforums.org/">Provide one-on-one support
-        <div class="extra">for the Ubuntu Forums</div>
-      </li>
-      <li target="http://askubuntu.com/">Answer questions
-        <div class="extra">at AskUbuntu</div>
-      </li>
-      <li target="http://askubuntu.com/">Edit and refine answers
-        <div class="extra">at AskUbuntu</div>
-      </li>
-      <li target="http://ubuntuforums.org/forumdisplay.php?f=100">Compile tutorials
-        <div class="extra">for the Ubuntu Forums</div>
-      </li>
-      <li target="http://answers.launchpad.com/">Compile FAQs
-        <div class="extra">at Launchpad Answers</div>
-      </li>
-      <li target="http://community.ubuntu.com/contribute/documentation/">Improve documentation
-        <div class="extra">with the Ubuntu Documentation Team</div>
-      </li>
-      <li target="https://wiki.ubuntu.com/Classroom">Teach
-        <div class="extra">Share your knowledge and skills in Ubuntu Classroom</div>
-      </li>
-    </ul>
-  </div>
-
-
-  <div id="responses">
-    <div id="ok"><a href="javascript:">Tell me more</a></div>
-    <div id="next">
-      <a href="javascript:">
-        <span class="negative">No, not interested</span>
-        <span class="negative">Show me something else</span>
-        <span class="negative">Boring</span>
-        <span class="negative">Pffft, whatever</span>
-        <span class="negative">Not my line of expertise</span>
-        <span class="negative">Next please</span>
-      </a>
+    <div class="group" id="toplevel">
+      <p class="question">How would you like to help?<br/>What scratches your itch?</p>
+      <ul class="choices">
+        <li next-group="advocate"><h2>Advocacy</h2></li>
+        <li next-group="communicate"><h2>Writing and editing</h2></li>
+        <li next-group="develop"><h2>Coding and development</h2></li>
+        <li next-group="quality"><h2>Quality and design</h2></li>
+        <li next-group="support"><h2>Support</h2></li>
+      </ul>
     </div>
-    <div id="back"><a href="javascript:">I was wrong, take me back</a></div>
-
-    <!-- message showing only if javascript disabled -->
-    <noscript>
-        <span>Please enable JavaScript in your browser!</span>
-    </noscript>
-    <!-- end of noscript message -->
-
-  </div>
 
 
-</div> <!-- Volunteer Wizard -->
+
+    <div class="group" id="advocate">
+      <p class="question">So you want to well the world about Ubuntu?<br/>You could help</p>
+      <ul class="choices">
+        <li target="http://www.spreadubuntu.org/en/spreadubuntu-our-project"><h2>Create</h2>
+          <p class="extra">Create materials for the Spreadubuntu project</p>
+        </li>
+        <li target="http://www.spreadubuntu.org/"><h2>Persuade</h2>
+          <p class="extra">Evangelize with the Spreadubuntu project</p>
+        </li>
+        <li target="https://wiki.ubuntu.com/StartUbuntu#HOWTO_Help.3F"><h2>Convert WinXP users</h2>
+          <p class="extra">Evangelize with the Startubuntu project</p>
+        </li>
+      </ul>
+    </div>
+
+
+
+    <div class="group" id="communicate">
+      <p class="question">So you have a mighty pen?<br/>You could help</p>
+      <ul class="choices">
+        <li target="https://wiki.ubuntu.com/UbuntuWeeklyNewsletter/Join"><h2>Interview</h2>
+          <p class="extra">Get the message across in the Ubuntu Weekly News</p>
+        </li>
+        <li target="https://wiki.ubuntu.com/Classroom"><h2>Teach
+          <p class="extra">Share your knowledge and skills in Ubuntu Classroom</p>
+        </li>
+        <li target="http://community.ubuntu.com/contribute/translations/"><h2>Translate</h2>
+          <p class="extra">Help make Ubuntu accessible to everyone</p>
+        </li>
+        <li target="https://wiki.ubuntu.com/DocumentationTeam"><h2>Edit and update documentation</h2>
+          <p class="extra">with the Ubuntu Documentation Team</p>
+        </li>
+        <li target="https://wiki.ubuntu.com/UbuntuWeeklyNewsletter/Join"><h2>Write/edit news copy</h2>
+          <p class="extra">for the Ubuntu Weekly News</p>
+        </li>
+        <li target="http://ubuntuforums.org/forumdisplay.php?f=100"><h2>Compile tutorials</h2>
+          <p class="extra">for the Ubuntu Forums</p>
+        </li>
+        <li target="http://askubuntu.com/"><h2>Edit</h2>
+          <p class="extra">questions and answers at AskUbuntu</p>
+        </li>
+      </ul>
+    </div>
+
+
+
+    <div class="group" id="develop">
+      <p class="question">So you want to change the world with great software?<br/>You could help</p>
+      <ul class="choices">
+        <li target="https://wiki.ubuntu.com/QATeam/Roles/BugTriager"><h2>Triage bugs</h2>
+          <p class="extra">with the Ubuntu QA Team</p>
+        </li>
+        <li target="https://wiki.ubuntu.com/One%20Hundred%20Papercuts"><h2>Patch simple bugs</h2>
+          <p class="extra">with the One Hundred Papercuts Project</p>
+        </li>
+        <li target="http://developer.ubuntu.com/"><h2>Develop applications</h2>
+          <p class="extra">for Cloud, Server, Desktop, Tablet, and/or Phone</p>
+        </li>
+        <li target="https://wiki.ubuntu.com/FoundationsTeam"><h2>Improve the core OS</h2>
+          <p class="extra">with the Ubuntu Foundations Team</p>
+        </li>
+        <li target="https://wiki.ubuntu.com/QATeam/Roles/Developer"><h2>Create infrastructure tools</h2>
+          <p class="extra">with the Ubuntu QA Team</p>
+        </li>
+        <li target="http://packaging.ubuntu.com/"><h2>Package software</h2>
+          <p class="extra">to add to Ubuntu</p>
+        </li>
+        <li target="https://wiki.ubuntu.com/SecurityTeam/GettingInvolved"><h2>Protect millions</h2>
+          <p class="extra">with the Ubuntu Security Team</p>
+        </li>
+        <li target="https://wiki.ubuntu.com/QATeam/Roles/TestWriter"><h2>Write automated tests</h2>
+          <p class="extra">with the Ubuntu QA Team</p>
+        </li>
+        <li target="https://wiki.ubuntu.com/QATeam/Roles/Tester"><h2>Test the development release</h2>
+          <p class="extra">with the Ubuntu QA Team</p>
+        </li>
+        <li target="http://community.ubuntu.com/contribute/design/"><h2>Visual experience</h2>
+          <p class="extra">Wallpapers, art, and user experience design</p>
+        </li>
+      </ul>
+    </div>
+
+
+
+    <div class="group" id="quality">
+      <p class="question">So you enjoy finding programmer mistakes?<br/>You could help</p>
+      <ul class="choices">
+        <li target="https://wiki.ubuntu.com/FoundationsTeam"><h2>Improve the core OS</h2>
+          <p class="extra">with the Ubuntu Foundations Team</p>
+        </li>
+        <li target="http://community.ubuntu.com/contribute/design/"><h2>User experience</h2>
+          <p class="extra">Designing for the Desktop, Tablet, and Phone</p>
+        </li>
+        <li target="https://wiki.ubuntu.com/QATeam/Roles/Developer"><h2>Create infrastructure tools</h2>
+          <p class="extra">with the Ubuntu QA Team</p>
+        </li>
+        <li target="https://wiki.ubuntu.com/SecurityTeam/GettingInvolved"><h2>Protect millions</h2>
+          <p class="extra">with the Ubuntu Security Team</p>
+        </li>
+        <li target="https://wiki.ubuntu.com/QATeam/Roles/TestWriter"><h2>Write automated tests</h2>
+          <p class="extra">with the Ubuntu QA Team</p>
+        </li>
+        <li target="https://wiki.ubuntu.com/QATeam/Roles/Tester"><h2>Test the development release</h2>
+          <p class="extra">with the Ubuntu QA Team</p>
+        </li>
+        <li target="https://wiki.ubuntu.com/QATeam/Roles/BugTriager"><h2>Triage bugs</h2>
+          <p class="extra">with the Ubuntu QA Team</p>
+        </li>
+      </ul>
+    </div>
+
+
+
+    <div class="group" id="support">
+      <p class="question">So you like helping people?<br/>You could help</p>
+      <ul class="choices">
+        <li target="http://ubuntuforums.org/forumdisplay.php?f=100"><h2>Compile tutorials</h2>
+          <p class="extra">for the Ubuntu Forums</p>
+        </li>
+        <li target="http://ubuntuforums.org/"><h2>Provide one-on-one support</h2>
+          <p class="extra">for the Ubuntu Forums</p>
+        </li>
+        <li target="http://askubuntu.com/"><h2>Answer questions</h2>
+          <p class="extra">at AskUbuntu</p>
+        </li>
+        <li target="http://askubuntu.com/"><h2>Edit and refine answers</h2>
+          <p class="extra">at AskUbuntu</p>
+        </li>
+        <li target="http://ubuntuforums.org/forumdisplay.php?f=100"><h2>Compile tutorials</h2>
+          <p class="extra">for the Ubuntu Forums</p>
+        </li>
+        <li target="http://answers.launchpad.com/"><h2>Compile FAQs</h2>
+          <p class="extra">at Launchpad Answers</p>
+        </li>
+        <li target="http://community.ubuntu.com/contribute/documentation/"><h2>Improve documentation</h2>
+          <p class="extra">with the Ubuntu Documentation Team</p>
+        </li>
+        <li target="https://wiki.ubuntu.com/Classroom"><h2>Teach</h2>
+          <p class="extra">Share your knowledge and skills in Ubuntu Classroom</p>
+        </li>
+      </ul>
+    </div>
+
+
+    <div id="responses">
+      <div id="ok"><a href="javascript:">Tell me more</a></div>
+      <div id="next">
+        <a href="javascript:">
+          <span class="negative">No, not interested</span>
+          <span class="negative">Show me something else</span>
+          <span class="negative">Boring</span>
+          <span class="negative">Pffft, whatever</span>
+          <span class="negative">Not my line of expertise</span>
+          <span class="negative">Next please</span>
+        </a>
+      </div>
+      <div id="back"><a href="javascript:">I was wrong, take me back</a></div>
+      <!-- message showing only if javascript disabled -->
+      <noscript>
+          <span>Please enable JavaScript in your browser!</span>
+      </noscript>
+      <!-- end of noscript message -->
+    </div>
+
+  </div> <!-- volunteer_wizard -->
+
